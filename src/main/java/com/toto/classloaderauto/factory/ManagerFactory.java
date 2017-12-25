e9bab17eed48e19d74696396ac96d08561aa1a2e
toto-wen <wen18408253283@163.com>
2017-12-26 07:31:34 +0800

    1修改CacheThreadPool.java类来测试java线程池newCacheThreadPool 2新添classloaderauto包，包下面编写的是关于java热加载的简单实现方式，包括 LoadInfo.java,MyClassLoader.java,BaseManager.java,MyManager.java等五个类的变动

---
+++ src/main/java/com/toto/classloaderauto/factory/ManagerFactory.java
@@ -0,0 +1,81 @@
+package com.toto.classloaderauto.factory;
+
+import com.toto.classloaderauto.entity.LoadInfo;
+import com.toto.classloaderauto.entity.MyClassLoader;
+import com.toto.classloaderauto.inter.BaseManager;
+
+import java.io.File;
+import java.lang.reflect.InvocationTargetException;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @ClassName: ManagerFactory
+ * @Description: 加载manager的工厂
+ * @Author: wenpandong
+ * @date: 2017/12/25 22:23
+ */
+public class ManagerFactory {
+
+    /**
+     * 记录热加载类的加载信息
+     */
+    private static final Map<String,LoadInfo> loadTimeMap=new HashMap<String, LoadInfo>();
+
+    /**
+     * 要加载的类的classPath
+     */
+    public static final String CLASS_PATH="E:/ideaWorkContent/chat_room/out/production/chat_room";
+
+    /**
+     * 实现热加载的类的全名称(包名+类名)
+     */
+    public static final String MY_MANAGER="com.toto.classloaderauto.inter.MyManager";
+
+    public static BaseManager getManager(String className){
+        //加载类路径文件
+        File loadFile=new File(CLASS_PATH+className.replaceAll("\\.","/")+".class");
+        //
+        Long lastModified=loadFile.lastModified();
+        /**
+         * loadTimeMap不包含className为key的loadInfo信息，证明这个类没有被加载，那么需要加载这个类到JVM
+         */
+        if(loadTimeMap.get(className) == null){
+            load(className, lastModified);
+        }else if(loadTimeMap.get(className).getLoadTime()!=lastModified) {//加载类的时间戳变化了，我们同样重新加载这个类到JVM
+            load(className,lastModified);
+        }
+        return null;
+    }
+
+    private static void load(String className, Long lastModified) {
+        MyClassLoader myClassLoader = new MyClassLoader(CLASS_PATH);
+        Class<?> loadClass=null;
+        try {
+            loadClass = myClassLoader.loadClass(className);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        BaseManager manager = newInstance(loadClass);
+        LoadInfo loadInfo=new LoadInfo(myClassLoader,lastModified);
+        loadInfo.setBaseManager(manager);
+
+        loadTimeMap.put(className,loadInfo);
+    }
+
+    private static BaseManager newInstance(Class<?> loadClass) {
+
+        try {
+            return (BaseManager) loadClass.getConstructor(new Class[]{}).newInstance(new Object[]{});
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
